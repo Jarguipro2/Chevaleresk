3a4650fd164b5235d4d6f4973bcb2c1c46358dc8
Guillaume Jarry <201927061@edu.clg.qc.ca>
2021-03-25 10:37:40 -0400

    Ajoutez des fichiers projet.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Chevaleresk.Models;
+
+namespace Chevaleresk.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ChevalereskDBEntities2 db = new ChevalereskDBEntities2();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = db.Items.Include(i => i.Items_Type);
+            return View(items.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Items items = db.Items.Find(id);
+            if (items == null)
+            {
+                return HttpNotFound();
+            }
+            return View(items);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name");
+            return View();
+        }
+
+        // POST: Items/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] Items items)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(items);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", items.IdType);
+            return View(items);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Items items = db.Items.Find(id);
+            if (items == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", items.IdType);
+            return View(items);
+        }
+
+        // POST: Items/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] Items items)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(items).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", items.IdType);
+            return View(items);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Items items = db.Items.Find(id);
+            if (items == null)
+            {
+                return HttpNotFound();
+            }
+            return View(items);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Items items = db.Items.Find(id);
+            db.Items.Remove(items);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chevaleresk.MvcApplication" Language="C#" %>
