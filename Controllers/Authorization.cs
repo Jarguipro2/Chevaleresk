e94318529b30764e3ba952c061f2766ecc0070bf
AlexandreTardif <74985691+TobyLeRoi@users.noreply.github.com>
2021-04-01 23:22:40 -0400

    Finition de création joueur + connection joueur (déconnection à venir sous peu)

---
+++ Controllers/Authorization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chevaleresk.Models;
+
+namespace Chevaleresk.Controllers
+{
+    public class Authorization
+    {
+        public class PlayersAccess : AuthorizeAttribute
+        {
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                if (OnlinePlayers.GetSessionPlayer() != null)
+                    return true;
+                else
+                    httpContext.Response.Redirect("/Players/Login");
+                return base.AuthorizeCore(httpContext);
+            }
+        }
+        public class AdminAccess : AuthorizeAttribute
+        {
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                Players sessionPlayer = OnlinePlayers.GetSessionPlayer();
+                if(sessionPlayer != null)
+                    if (sessionPlayer.Admin)
+                        return true;
+                    else
+                        httpContext.Response.Redirect("/Players/Login");
+                return base.AuthorizeCore(httpContext);
+            }
+        }
+    }
+}
---
+++ Models/LoginView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chevaleresk.Models
+{
+    public class LoginView
+    {
+        [Required(ErrorMessage = "Requis")]
+        [Display(Name = "Nom d'utilisateur")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Requis")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
