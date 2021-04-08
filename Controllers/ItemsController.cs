f2f2ced188cebc1fa4fa0d4b53b13420a3fc300e
Guillaume Jarry <201927061@edu.clg.qc.ca>
2021-04-07 23:49:55 -0400

    Added Items MVC

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EFA_DEMO.Models;
+
+namespace EFA_DEMO.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private DBEntities2 db = new DBEntities2();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            List<Item> items = db.Items.ToList();
+            return View(items);
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] ItemView itemView)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AddItem(itemView);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemView);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/ItemView.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFA_DEMO.Models
+{
+    public class ItemView
+    {
+        public int IdObject { get; set; }
+        public string Name { get; set; }
+        public int StockQuantity { get; set; }
+        public double Price { get; set; }
+        public string PictureGUID { get; set; }
+        public int IdType { get; set; }
+        public virtual Items_Type Items_Type { get; set; }
+
+        public int StringToId(string type)
+        {
+            int valeur = default;
+            if (type.Equals("arme"))
+            {
+                valeur = 0;
+            }
+            else if (type.Equals("armure"))
+            {
+                valeur = 1;
+            }
+            else if (type.Equals("potion"))
+            {
+                valeur = 2;
+            }
+            else if (type.Equals("ressource"))
+            {
+                valeur = 3;
+            }
+            return valeur;
+        }
+        public Item ToItem()
+        {
+            return new Item()
+            {
+                IdObject = this.IdObject,
+                Name = this.Name,
+                StockQuantity = this.StockQuantity,
+                Price = this.Price,
+                PictureGUID = this.PictureGUID,
+                IdType = this.IdType,
+                Items_Type = this.Items_Type
+            };
+        }
+    }
+    public static class MyExtension
+    {
+        public static ItemView ToItemView(this Item item)
+        {
+            return new ItemView()
+            {
+                IdObject = item.IdObject,
+                Name = item.Name,
+                StockQuantity = item.StockQuantity,
+                Price = item.Price,
+                PictureGUID = item.PictureGUID,
+                IdType = item.IdType,
+                Items_Type = item.Items_Type                
+            };
+        }
+        public static ItemView AddItem(this DBEntities2 DB, ItemView itemView)
+        {
+            Item item = itemView.ToItem();
+            item = DB.Items.Add(item);
+            DB.SaveChanges();
+            return item.ToItemView();
+        }
+        public static bool UpdateItem(this DBEntities2 DB, ItemView itemView)
+        {
+            Item itemToUpdate = itemView.ToItem();
+            DB.Entry(itemToUpdate).State = EntityState.Modified;
+            DB.SaveChanges();
+            return true;
+        }
+        public static bool RemoveItem(this DBEntities2 DB, ItemView itemView)
+        {
+            Item itemToDelete = itemView.ToItem();
+            DB.Items.Remove(itemToDelete);
+            DB.SaveChanges();
+            return true;
+        }
+    }
+}
