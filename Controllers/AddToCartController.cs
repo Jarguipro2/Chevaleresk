be7e029af8dd3da53511ef0f9b4ee5b72564121e
Samuel M <73625335+sam00583@users.noreply.github.com>
2021-04-15 00:26:20 -0400

    Deleting actionresult qui servent a rien, modification MyOrder.

--- Controllers/AddToCartController.cs
+++ Controllers/AddToCartController.cs
@@ -64,31 +64,22 @@ namespace EFA_DEMO.Controllers
 
         public ActionResult Myorder()
         {
-            // --- Version si le user essaie de force l'entré ALPHA 0.01 ---
-            //var currentPlayer = null;
-
-            //if (OnlineUsers.CurrentUser != null)
-            //{
-            //    currentPlayer = db.Users.Find(OnlineUsers.CurrentUser.Id);
-            //}
-            // ------------------------------------------------------------
-
-            //Dictionary<Item, int> openWith =
-            //    new Dictionary<Item, int>();
-
             var currentPlayer = db.Users.Find(OnlineUsers.CurrentUser.Id);
 
             ViewBag.currentMoneyPlayer = currentPlayer.Money;
 
-            int nombreItemsSession = 0;
             double soldeTotal = 0;
+          
+            var query = (Dictionary<Item, int>)Session["cart"];
 
-            if ((Dictionary<Item, int>)Session["cart"] != null)
+            if (query != null)
             {
-                var query = (Dictionary<Item, int>)Session["cart"];
-
-              
+                foreach (var price in query)
+                {
+                    soldeTotal += price.Key.Price * price.Value;
+                }
             }
+            
             if (soldeTotal >= currentPlayer.Money)
             {
                 ViewBag.NotEnoughMoney = "Vous n'avez pas assez d'argent, pour compléter la transaction";
@@ -119,9 +110,6 @@ namespace EFA_DEMO.Controllers
                         itemDB.StockQuantity -= itemsReceive.Value;
                         currentPlayer.Money -= (int)soldeTotal;
 
-                        //db.UpdateUser(currentPlayer.ToUserView());
-                        db.Entry(itemDB).State = EntityState.Modified;
-
                         var invIdStatus = db.User_Inventory.SingleOrDefault(m => m.IdPlayer == currentPlayer.IdPlayer);
                         var idPlayerStatus = db.User_Inventory.SingleOrDefault(m => m.IdPlayer == currentPlayer.IdPlayer);
                         
@@ -148,113 +136,6 @@ namespace EFA_DEMO.Controllers
             }
 
             return RedirectToAction("MyOrder", "AddToCart");
-        }
-
-
-        // GET: AddToCart
-        public ActionResult Index()
-        {
-            var items = db.Items.Include(i => i.Items_Type);
-            return View(items.ToList());
-        }
-
-        // GET: AddToCart/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Item item = db.Items.Find(id);
-            if (item == null)
-            {
-                return HttpNotFound();
-            }
-            return View(item);
-        }
-
-        // GET: AddToCart/Create
-        public ActionResult Create()
-        {
-            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name");
-            return View();
-        }
-
-        // POST: AddToCart/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] Item item)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Items.Add(item);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", item.IdType);
-            return View(item);
-        }
-
-        // GET: AddToCart/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Item item = db.Items.Find(id);
-            if (item == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", item.IdType);
-            return View(item);
-        }
-
-        // POST: AddToCart/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdObject,Name,StockQuantity,Price,PictureGUID,IdType")] Item item)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(item).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.IdType = new SelectList(db.Items_Type, "IdType", "Name", item.IdType);
-            return View(item);
-        }
-
-        // GET: AddToCart/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Item item = db.Items.Find(id);
-            if (item == null)
-            {
-                return HttpNotFound();
-            }
-            return View(item);
-        }
-
-        // POST: AddToCart/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Item item = db.Items.Find(id);
-            db.Items.Remove(item);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
