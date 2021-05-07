7026a69909ca9c2fd87b68444835d6e42c469152
AlexandreTardif <74985691+TobyLeRoi@users.noreply.github.com>
2021-05-07 16:06:24 -0400

    Ajout propriété

--- Models/ModelDB.Designer.cs
+++ Models/ModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Guillaume\Desktop\Chevaleresk\Models\ModelDB.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Alexandre Tardif\Desktop\École\SessionH2021\Projet dirigé\Chevaleresk-2.2\Models\ModelDB.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
